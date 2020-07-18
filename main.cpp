ccff98acbf65aeeb0d706776381751c067b988dc
yyuanl <1092068432@qq.com>
2020-07-19 00:58:30 +0800

    7.19

--- main.cpp
+++ main.cpp
@@ -1,82 +1,156 @@
-#include<libgen.h>
-#include<stdio.h>
-#include<stdlib.h>
-#include<sys/un.h>
-#include<sys/socket.h>
-#include<arpa/inet.h>
-#include<assert.h>
-#include<sys/types.h>
-#include<sys/epoll.h>
-#include<pthread.h>
-#include"./ioProcessUnit/http.h"
-#include"./logicalProcessUnit/work.h"
-//#define NDEBUG
-#define MAX_EVENT_NUMBER 1024
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <cassert>
+#include <sys/epoll.h>
 
-int main(int argc, char *argv[]){
-    if(argc < 3){
-        printf("usage:%s please input ip adrress and port\n", basename(argv[0]));
+#include "./lock/locker.h"
+#include "./threadPool/threadpool.h"
+#include "./ioProcessUnit/http_conn.h"
+
+#define MAX_FD 65536
+#define MAX_EVENT_NUMBER 10000
+
+extern int addfd( int epollfd, int fd, bool one_shot );
+extern int removefd( int epollfd, int fd );
+
+void addsig( int sig, void( handler )(int), bool restart = true )
+{
+    struct sigaction sa;
+    memset( &sa, '\0', sizeof( sa ) );
+    sa.sa_handler = handler;
+    if( restart )
+    {
+        sa.sa_flags |= SA_RESTART;
     }
-    struct epoll_event okevents[MAX_EVENT_NUMBER]; // for prepared event;
-    // set socket address, and socket() -> bind() -> listen()
-    const char *ip = argv[1];
-    int port  = atoi(argv[2]);
-    struct sockaddr_in addrServer;
-    addrServer.sin_family = AF_INET;
-    inet_pton(AF_INET, ip, &addrServer.sin_addr);
-    addrServer.sin_port = htons(port);
-    
-    int listfd = socket(PF_INET, SOCK_STREAM,0);
-    assert(listfd != -1);
- 
-    int ret = bind(listfd, (struct sockaddr * )&addrServer, sizeof(addrServer));
-    assert(ret != -1);
-    ret = listen(listfd, 5);
+    sigfillset( &sa.sa_mask );
+    assert( sigaction( sig, &sa, NULL ) != -1 );
+}
 
-    int eventTable = epoll_create(5);
-    assert(eventTable != -1);
-    addfd(eventTable, listfd, false); // can't register listen fd with EPOLLONESHOT
-#ifndef NDEBUG
-    printf("%s function :successfuly register listen fd %d at %s.\n", __func__,listfd, __DATE__);
-#endif
-    while(1){
-        int ret = epoll_wait(eventTable, okevents, MAX_EVENT_NUMBER,-1);
-        if(ret < 0){
-            printf("epoll failure\n");
+void show_error( int connfd, const char* info )
+{
+    printf( "%s", info );
+    send( connfd, info, strlen( info ), 0 );
+    close( connfd );
+}
+
+
+int main( int argc, char* argv[] )
+{
+    if( argc <= 2 )
+    {
+        printf( "usage: %s ip_address port_number\n", basename( argv[0] ) );
+        return 1;
+    }
+    const char* ip = argv[1];
+    int port = atoi( argv[2] );
+
+    addsig( SIGPIPE, SIG_IGN );
+
+    threadpool< http_conn >* pool = NULL;
+    try
+    {
+        pool = new threadpool< http_conn >;
+    }
+    catch( ... )
+    {
+        return 1;
+    }
+
+    http_conn* users = new http_conn[ MAX_FD ];
+    assert( users );
+    int user_count = 0;
+
+    int listenfd = socket( PF_INET, SOCK_STREAM, 0 );
+    assert( listenfd >= 0 );
+    struct linger tmp = { 1, 0 };
+    setsockopt( listenfd, SOL_SOCKET, SO_LINGER, &tmp, sizeof( tmp ) );
+
+    int ret = 0;
+    struct sockaddr_in address;
+    bzero( &address, sizeof( address ) );
+    address.sin_family = AF_INET;
+    inet_pton( AF_INET, ip, &address.sin_addr );
+    address.sin_port = htons( port );
+
+    ret = bind( listenfd, ( struct sockaddr* )&address, sizeof( address ) );
+    assert( ret >= 0 );
+
+    ret = listen( listenfd, 5 );
+    assert( ret >= 0 );
+
+    epoll_event events[ MAX_EVENT_NUMBER ];
+    int epollfd = epoll_create( 5 );
+    assert( epollfd != -1 );
+    addfd( epollfd, listenfd, false );
+    http_conn::m_epollfd = epollfd;
+
+    while( true )
+    {
+        int number = epoll_wait( epollfd, events, MAX_EVENT_NUMBER, -1 );
+        if ( ( number < 0 ) && ( errno != EINTR ) )
+        {
+            printf( "epoll failure\n" );
             break;
         }
-        for(int i = 0; i < ret; i++){
-            int currfd = okevents[i].data.fd;
-            if(currfd == listfd){
-                struct sockaddr_in reqClientAddr;
-                socklen_t reqClientAddLen = sizeof(reqClientAddr);
-                int connfd = accept(listfd, (struct sockaddr *)&reqClientAddr, &reqClientAddLen);
-                assert(connfd != -1);
-                addfd(eventTable, connfd, true); // register connected socket fd with EPOLLONESHOT
 
-#ifndef NDEBUG
-                printf("%s function :successfuly register connect fd %d at %s.\n", __func__,connfd, __DATE__);
-#endif               
-            }else if(okevents[i].events & EPOLLIN){
-                pthread_t thread;
-                fds newWorkFds;
-                newWorkFds.epollEveTab = eventTable;
-                newWorkFds.sockfd = currfd;
-                pthread_create(&thread, NULL, worker, (void *) &newWorkFds);
-            }else{
-                printf("something else happened \n");
+        for ( int i = 0; i < number; i++ )
+        {
+            int sockfd = events[i].data.fd;
+            if( sockfd == listenfd )
+            {
+                struct sockaddr_in client_address;
+                socklen_t client_addrlength = sizeof( client_address );
+                int connfd = accept( listenfd, ( struct sockaddr* )&client_address, &client_addrlength );
+                if ( connfd < 0 )
+                {
+                    printf( "errno is: %d\n", errno );
+                    continue;
+                }
+                if( http_conn::m_user_count >= MAX_FD )
+                {
+                    show_error( connfd, "Internal server busy" );
+                    continue;
+                }
+                
+                users[connfd].init( connfd, client_address );
             }
+            else if( events[i].events & ( EPOLLRDHUP | EPOLLHUP | EPOLLERR ) )
+            {
+                users[sockfd].close_conn();
+            }
+            else if( events[i].events & EPOLLIN )
+            {
+                if( users[sockfd].read() )
+                {
+                    pool->append( users + sockfd );
+                }
+                else
+                {
+                    users[sockfd].close_conn();
+                }
+            }
+            else if( events[i].events & EPOLLOUT )
+            {
+                if( !users[sockfd].write() )
+                {
+                    users[sockfd].close_conn();
+                }
+            }
+            else
+            {}
         }
     }
 
-    
-
-
-    close(listfd);
-#ifndef NDEBUG
-    printf("service stop..\n");
-#endif
-
+    close( epollfd );
+    close( listenfd );
+    delete [] users;
+    delete pool;
     return 0;
-
 }
